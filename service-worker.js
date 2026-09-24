a3170a703029ede06e63dfe7d82c51d51f97b958
Walterudisson <101980475+Walterudisson@users.noreply.github.com>
2026-09-24 15:56:02 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const VERSAO = 'v1.13.9-homologacao';
+const VERSAO = 'v1.13.9-h1-homologacao';
 const CACHE_SHELL = `cmapp-shell-${VERSAO}`;
 const CACHE_RUNTIME = `cmapp-runtime-${VERSAO}`;
 const PREFIXO_CACHE = 'cmapp-';
